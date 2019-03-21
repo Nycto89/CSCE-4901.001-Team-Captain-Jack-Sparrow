8cf55d2360b3b145555c168e15537d87a65cf6f5
nycto89 <nycto89@gmail.com>
2019-03-21 14:43:14 -0500

    moved olivia_menu into home_nav/pages/Modalities.js

--- App.js
+++ App.js
@@ -11,19 +11,6 @@ import React, {Component} from 'react';
 import {Animated, TouchableHighlight, Dimensions, 
 Platform, StyleSheet, Text, 
 View, Image, PanResponder } from 'react-native';
-import Swiper from 'react-native-swiper';
-import SplashScreen from './splashScreen/index';
-import HomeScreen from './home';
-import Transplant from './modalities/transplant';
-import Peritoneal from './modalities/peritoneal';
-import Incenter from './modalities/incenter';
-import Overnight from './modalities/overnight';
-import HomeHemo from './modalities/homehemo';
-import Therapy from './modalities/ctherapy';
-import DrawerMenu from './drawers'
-import {createStackNavigator, createDrawerNavigator, 
-createAppContainer, DrawerItems, DrawerActions,
-CardStackStyleInterpolator} from 'react-navigation';
 import {Router, Scene, Actions} from 'react-native-router-flux';
 
 import AppContainer from './Navigator.js';
@@ -53,66 +40,6 @@ var HomeButton = function() {
             </TouchableHighlight>
           </View>
     )
-}
-
-export class drawerApp extends Component {
-
-  render() {
-    return(
-      <Router>
-        <Scene key= "root">
-          <Scene key="mainScreen" 
-          component={HomeScreen} 
-          title="Welcome!"
-          renderBackButton={()=>(null)}
-          panHandlers = {null}
-          initial 
-          />
-
-          <Scene 
-          key="drawer"
-          drawer
-          contentComponent={DrawerMenu}
-          drawerWidth={300}
-          panHandlers = {null}
-          hideNavBar>
-
-            <Scene
-                key="drawer1"
-                title="Kidney Transplant"
-                component={Transplant}
-                renderRightButton = {HomeButton}/>
-            <Scene
-                key="drawer2"
-                title="Peritoneal Dialysis"
-                component={Peritoneal}
-                renderRightButton = {HomeButton}/>
-            <Scene
-                key="drawer3"
-                title="In-Center Hemodialysis"
-                component={Incenter}
-                renderRightButton = {HomeButton}/>
-            <Scene
-                key="drawer4"
-                title="Nocturnal Dialysis"
-                component={Overnight}
-                renderRightButton = {HomeButton}/>
-            <Scene
-                key="drawer5"
-                title="Home Hemodialysis"
-                component={HomeHemo}
-                renderRightButton = {HomeButton}/>
-            <Scene
-                key="drawer6"
-                title="Conservative Therapy"
-                component={Therapy}
-                renderRightButton = {HomeButton}/>
-          </Scene>
-        </Scene>
-      </Router>
-      //<AppNav />
-    );
-  }
 }
 
 const styles = StyleSheet.create({
--- pages/Modalities.js
+++ pages/Modalities.js
@@ -1,20 +1,78 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+// import Swiper from 'react-native-swiper';
+// import SplashScreen from './splashScreen/index';
+import HomeScreen from './home';
+import Transplant from './modalities/transplant';
+import Peritoneal from './modalities/peritoneal';
+import Incenter from './modalities/incenter';
+import Overnight from './modalities/overnight';
+import HomeHemo from './modalities/homehemo';
+import Therapy from './modalities/ctherapy';
+import DrawerMenu from './drawers'
+// import {
+//   createStackNavigator, createDrawerNavigator,
+//   createAppContainer, DrawerItems, DrawerActions,
+//   CardStackStyleInterpolator
+// } from 'react-navigation';
 
 export default class Modalities extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Modalities Page!</Text>
-        <Button
-          title="Back to home"
-          onPress={() =>
-            this.props.navigation.navigate('Home')
-          }
-        />
-      </View>
-    );
-  }
+      render() {
+      return (
+        <Router>
+          <Scene key="root">
+            <Scene key="mainScreen"
+              component={HomeScreen}
+              title="Welcome!"
+              renderBackButton={() => (null)}
+              panHandlers={null}
+              initial
+            />
+
+            <Scene
+              key="drawer"
+              drawer
+              contentComponent={DrawerMenu}
+              drawerWidth={300}
+              panHandlers={null}
+              hideNavBar>
+
+              <Scene
+                key="drawer1"
+                title="Kidney Transplant"
+                component={Transplant}
+                renderRightButton={HomeButton} />
+              <Scene
+                key="drawer2"
+                title="Peritoneal Dialysis"
+                component={Peritoneal}
+                renderRightButton={HomeButton} />
+              <Scene
+                key="drawer3"
+                title="In-Center Hemodialysis"
+                component={Incenter}
+                renderRightButton={HomeButton} />
+              <Scene
+                key="drawer4"
+                title="Nocturnal Dialysis"
+                component={Overnight}
+                renderRightButton={HomeButton} />
+              <Scene
+                key="drawer5"
+                title="Home Hemodialysis"
+                component={HomeHemo}
+                renderRightButton={HomeButton} />
+              <Scene
+                key="drawer6"
+                title="Conservative Therapy"
+                component={Therapy}
+                renderRightButton={HomeButton} />
+            </Scene>
+          </Scene>
+        </Router>
+        //<AppNav />
+      );
+    }
 }
 
 const styles = StyleSheet.create({
