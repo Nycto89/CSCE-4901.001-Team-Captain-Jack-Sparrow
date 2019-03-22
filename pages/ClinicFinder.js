4b9a8a2ac07b0a93e4e7ff275852fcba07602f46
CrazyKidJack <jackpfeffer@yahoo.com>
2019-03-22 10:37:37 -0500

    clinic list commit

--- pages/ClinicFinder.js
+++ pages/ClinicFinder.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component } from 'react';
+import { Text, StyleSheet, ScrollView, View} from 'react-native';
+import {clinicLst} from '../clinicData/clinicLookupData';
 
-export default class ClinicFinder extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Clinic Finder Page!</Text>
-        <Button
-          title="Back to home"
-          onPress={() =>
-            this.props.navigation.navigate('Home')
-          }
-        />
+export default class ScrollViewTEST extends Component{
+  constructor(){
+    super()
+    this.state={
+      firstVar:'',
+      listValue:[
+        {'name': 'list1', 'id': 1},
+        {'name': 'list2', 'id': 2},
+        {'name': 'list3', 'id': 3},
+        {'name': 'list4', 'id': 4},
+        {'name': 'list5', 'id': 5},
+        {'name': 'list6', 'id': 6},
+        {'name': 'list7', 'id': 7},
+        {'name': 'list8', 'id': 8},
+        {'name': 'list9', 'id': 9},
+        {'name': 'list10', 'id': 10},
+        {'name': 'list11', 'id': 11},
+        {'name': 'list12', 'id': 12}
+      ]
+    }
+  }
+
+  makeList=(item)=>(
+    <View key={item.id} style={styles.listStyle}>
+      <Text style={styles.nameStyle}>{item.name}</Text>
+      <Text>{item.addr}</Text>
+    </View>
+  );
+
+  render(){
+    return(
+      <View /*style={styles.mainViewStyle}*/>
+        <Text>Dallas Clinic List</Text>
+        <ScrollView>{clinicLst.map(this.makeList)}</ScrollView>
       </View>
     );
   }
-}
+}//end class ScrollViewTEST
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  listStyle:{
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 5,
+    paddingRight: 5,
+    margin: 2,
+    borderColor: '#2a4944',
+    borderWidth: 1,
+    backgroundColor: '#d2f7f1'
   },
-});
+  
+  nameStyle:{
+    fontWeight: 'bold'
+  }/*,
+
+  mainViewStyle:{
+    margin: 50, height: 5000
+  }*/
+})
