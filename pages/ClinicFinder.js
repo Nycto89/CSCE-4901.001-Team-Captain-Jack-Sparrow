1414fd81fe0bcc8b70d6ac129d15d8ec80f19b82
CrazyKidJack <jackpfeffer@yahoo.com>
2019-03-22 13:00:27 -0500

    open map with pin working

--- pages/ClinicFinder.js
+++ pages/ClinicFinder.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, ScrollView, View} from 'react-native';
+import { Text, Button, StyleSheet, ScrollView, View} from 'react-native';
+import {createOpenLink} from 'react-native-open-maps';
 import {clinicLst} from '../clinicData/clinicLookupData';
 
 export default class ScrollViewTEST extends Component{
-  constructor(){
-    super()
-    this.state={
-      firstVar:'',
-      listValue:[
-        {'name': 'list1', 'id': 1},
-        {'name': 'list2', 'id': 2},
-        {'name': 'list3', 'id': 3},
-        {'name': 'list4', 'id': 4},
-        {'name': 'list5', 'id': 5},
-        {'name': 'list6', 'id': 6},
-        {'name': 'list7', 'id': 7},
-        {'name': 'list8', 'id': 8},
-        {'name': 'list9', 'id': 9},
-        {'name': 'list10', 'id': 10},
-        {'name': 'list11', 'id': 11},
-        {'name': 'list12', 'id': 12}
-      ]
-    }
-  }
-
   makeList=(item)=>(
-    <View key={item.id} style={styles.listStyle}>
-      <Text style={styles.nameStyle}>{item.name}</Text>
-      <Text>{item.addr}</Text>
+    <View key={item.id} style={styles.rowStyle}>
+      <View style={styles.listStyle}>
+        <Text style={styles.nameStyle}>{item.name}</Text>
+        <Text>{item.addr}</Text>
+      </View>
+      <View style={styles.buttonStyle}>
+        <Button
+          color={'#0000ff'}
+          title="Map"
+          onPress={createOpenLink({query:(item.addr)})}
+        />
+      </View>
     </View>
   );
 
@@ -42,7 +31,17 @@ export default class ScrollViewTEST extends Component{
 }//end class ScrollViewTEST
 
 const styles = StyleSheet.create({
+  rowStyle:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderColor: '#2a4944',
+    borderWidth: 1,
+    backgroundColor: '#d2f7f1'
+  },
+  
   listStyle:{
+    alignSelf: 'flex-start',
     flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'flex-start',
@@ -50,10 +49,23 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
     paddingLeft: 5,
     paddingRight: 5,
+    margin: 2
+    //borderColor: '#2a4944',
+    //borderWidth: 1,
+    //backgroundColor: '#d2f7f1'
+  },
+
+  buttonStyle:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 5,
+    paddingRight: 5,
     margin: 2,
-    borderColor: '#2a4944',
-    borderWidth: 1,
-    backgroundColor: '#d2f7f1'
+    width: 100,
+    height: 100
   },
   
   nameStyle:{
