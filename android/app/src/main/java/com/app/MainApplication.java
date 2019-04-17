5bc0221b1752d1ec5b8caf21c4791982a14540be
Kevin Morales <kevinmm835@gmail.com>
2019-04-17 18:00:09 -0500

    Missed some things

--- android/app/src/main/java/com/app/MainApplication.java
+++ android/app/src/main/java/com/app/MainApplication.java
@@ -8,6 +8,8 @@ import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
+import com.levelasquez.androidopensettings.AndroidOpenSettingsPackage;
+import com.devfd.RNGeocoder.RNGeocoderPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -30,9 +32,11 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new OpenAppSettingsPackage(),
             new RNCameraPackage(),
+            new RNGeocoderPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
-            new RNFusedLocationPackage()
+            new RNFusedLocationPackage(),
+            new AndroidOpenSettingsPackage() 
       );
     }
 
