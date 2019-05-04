a235f977327ffeed550e444ff6a9847d893e6681
CrazyKidJack <jackpfeffer@yahoo.com>
2019-05-03 20:41:33 -0500

    updated drawer with animations/collapsing

--- drawer.js
+++ drawer.js
@@ -1,21 +1,48 @@
 import React, {Component} from 'react';
-import {Animated, TouchableHighlight, Dimensions, 
-Platform, StyleSheet, Text, 
-View, Image, PanResponder } from 'react-native';
+import {Animated, Easing, TouchableWithoutFeedback, Dimensions, Platform, StyleSheet, Text, View, Image, PanResponder } from 'react-native';
+import Accordion from 'react-native-collapsible/Accordion';
+import Collapsible from 'react-native-collapsible';
 import {Actions} from 'react-native-router-flux';
 import {Content, List, ListItem} from 'native-base';
 import {connect} from 'react-redux';
 
 class DrawerMenu extends React.Component {
+    state = {isCollapsed: true};
+    spinVal = new Animated.Value(0);
+    spin(){
+        this.spinVal.setValue(0);
+        Animated.timing(
+            this.spinVal,
+            {
+                toValue: 1,
+                duration: 500,
+                easing: Easing.bezier(.66, 0, .17, 1.52),
+                useNativeDriver: true
+            }
+        ).start();
+    }
+    
+    toggleCollapse(){
+        console.log('toggle');
+        console.log('isCollapsed: '+this.state.isCollapsed);
+        this.spin();
+        if(this.state.isCollapsed) this.setState({isCollapsed: false});
+        else    this.setState({isCollapsed: true});
+        console.log('done toggling...');
+    }
 
     componentDidMount() { Actions.refresh({key: 'Drawer', ref: this.refs.navigation}); }
 
     render() {
-
+        let spin;
+        if(this.state.isCollapsed)
+            spin = this.spinVal.interpolate({inputRange:[0,1], outputRange:["360deg", "0deg"]});
+        else
+            spin = this.spinVal.interpolate({inputRange:[0,1], outputRange:["45deg", "360deg"]});
         return (
                 <View style={[styles.drawerStyle, {backgroundColor: this.props.themeProp.backgroundColor}]}>
-                    <Content>
-                        <List>
+                    <Content style={{width: "100%"}}>
+                        <List style={{width: "100%"}}>
                             <ListItem onPress={() => Actions.HomeScreen({type : "reset" })}>
                                 <Text style={[styles.homeText, {color: this.props.themeProp.accentColor}]}>Return to Home</Text>
                             </ListItem>
@@ -28,26 +55,44 @@ class DrawerMenu extends React.Component {
                             <ListItem onPress={() => Actions.Nutrition()}>
                                 <Text style={{color: this.props.themeProp.textColor}}>Phosphorus Search</Text>
                             </ListItem>
-                            <ListItem onPress={() => Actions.ModalityMainScreen()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>Modalities</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.KidneyTransplant()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>   Kidney Transplant</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.PeritonealDialysis()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>   Peritoneal Dialysis</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.InCenterHemodialysis()}> 
-                                <Text style={{color: this.props.themeProp.textColor}}>   In-Center Hemodialysis</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.NocturnalDialysis()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>   Nocturnal Dialysis</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.HomeHemodialysis()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>   Home Hemodialysis</Text>
-                            </ListItem>
-                            <ListItem onPress={() => Actions.ConservativeTherapy()}>
-                                <Text style={{color: this.props.themeProp.textColor}}>   Conservative Therapy</Text>
+                            <ListItem style={{width: "100%", flexDirection: "column", alignItems: "center"}}onPress={() => Actions.ModalityMainScreen()}>
+                                <View style={{width: "100%", flexDirection: "row", alignSelf: "flex-start", alignItems: "center"}}>
+                                    <View>
+                                        <Text style={{color: this.props.themeProp.textColor}}>Modalities</Text>
+                                    </View>
+                                    <View style={{width: "30%", marginRight: "30%"}}>
+                                        <TouchableWithoutFeedback style={{width: "100%"}} onPress={() => this.toggleCollapse()}>
+                                            <View style={{width: "100%", justifyContent: "flex-start", alignItems: "center"}}>
+                                                <Animated.Text style={{fontSize: 17, 
+                                                    color: this.props.themeProp.textColor,
+                                                    transform: [{rotate: spin}]}}
+                                                >
+                                                    +
+                                                </Animated.Text>
+                                            </View>
+                                        </TouchableWithoutFeedback>
+                                    </View>
+                                </View>
+                                <Collapsible collapsed={this.state.isCollapsed}>
+                                    <ListItem onPress={() => Actions.KidneyTransplant()}>
+                                        <Text style={{color: this.props.themeProp.textColor}}>   Kidney Transplant</Text>
+                                    </ListItem>
+                                    <ListItem onPress={() => Actions.PeritonealDialysis()}>
+                                        <Text style={{color: this.props.themeProp.textColor}}>   Peritoneal Dialysis</Text>
+                                    </ListItem>
+                                    <ListItem onPress={() => Actions.InCenterHemodialysis()}> 
+                                        <Text style={{color: this.props.themeProp.textColor}}>   In-Center Hemodialysis</Text>
+                                    </ListItem>
+                                    <ListItem onPress={() => Actions.NocturnalDialysis()}>
+                                        <Text style={{color: this.props.themeProp.textColor}}>   Nocturnal Dialysis</Text>
+                                    </ListItem>
+                                    <ListItem onPress={() => Actions.HomeHemodialysis()}>
+                                        <Text style={{color: this.props.themeProp.textColor}}>   Home Hemodialysis</Text>
+                                    </ListItem>
+                                    <ListItem onPress={() => Actions.ConservativeTherapy()}>
+                                        <Text style={{color: this.props.themeProp.textColor}}>   Conservative Therapy</Text>
+                                    </ListItem>
+                                </Collapsible>
                             </ListItem>
                             <ListItem onPress={() => Actions.SettingsLightbox()}>
                                 <Text style={{color: this.props.themeProp.textColor}}>Settings</Text>
